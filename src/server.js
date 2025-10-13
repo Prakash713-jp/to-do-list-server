36c8dd280c852b67ed120474cc6774f95ee88e90
Prakash713-jp <jayaprakashkdmjp713@gmail.com>
2025-10-13 17:48:22 +0530

    link

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://prakash713-jp.github.io/To_Do_List/", // ← replace this
+    "https://prakash713-jp.github.io", // ← replace this
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
